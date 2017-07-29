cfbf3223c96df1edfc02338d88b59b3553c92a3a
Matt Williams <matwilliams@hotmail.com>
2017-07-29 23:38:16 +0100

    Now with a friend, and walking (WASD)

--- game.js
+++ game.js
@@ -4,10 +4,14 @@ function Game(canvas) {
 
   this.resize();
   var otherThis = this;
-  window.onresize = wrap(this, this.resize);
+  window.addEventListener("resize", wrap(this, this.resize));
 
   this.initTextures();
-  this.robot = new Robot(this.gl);
+  this.initControls();
+  this.robots = [
+    new Robot(this.gl, Robot.BLUE),
+    new Robot(this.gl, Robot.RED, 256, 0, -1),
+  ];
 
   this.draw();
 }
@@ -15,6 +19,21 @@ function Game(canvas) {
 Game.prototype.initTextures = function() {
   var gl = this.gl;
   Robot.init(gl);
+}
+
+Game.prototype.initControls = function() {
+  var otherThis = this;
+  this.keysDown = {};
+  window.addEventListener("keydown", function(evt) {
+    evt = evt || window.event;
+    evt.preventDefault();
+    otherThis.keysDown[String.fromCharCode(evt.keyCode)] = true;
+  }, false);
+  window.addEventListener("keyup", function(evt) {
+    evt = evt || window.event;
+    evt.preventDefault();
+    delete(otherThis.keysDown[String.fromCharCode(evt.keyCode)]);
+  }, false);
 }
 
 Game.prototype.resize = function() {
@@ -36,14 +55,30 @@ Game.prototype.draw = function() {
   gl.enable(gl.BLEND);
   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-  this.robot.draw(this.view);
+  if (this.keysDown['A']) {
+    this.robots[0].facing = -1;
+    this.robots[0].walking = true;
+  } else if (this.keysDown['D']) {
+    this.robots[0].facing = 1;
+    this.robots[0].walking = true;
+  } else {
+    this.robots[0].walking = false;
+  }
+
+  for (var ii = 0; ii < this.robots.length; ii++) {
+    this.robots[ii].draw(this.view);
+  }
 
   window.requestAnimationFrame(wrap(this, this.draw));
 }
 
-function Robot(gl) {
+function Robot(gl, parts, x, y, facing) {
   this.gl = gl;
-  this.tick = 0;
+  this.parts = parts;
+  this.facing = facing || 1;
+  this.tick = Math.random() * 40;
+  this.x = x || 0;
+  this.y = y || 0;
 }
 
 Robot.init = function(gl) {
@@ -64,10 +99,21 @@ Robot.init = function(gl) {
      "  gl_FragColor = texture2D(texture, uv2);",
      "}"]);
 
-  Robot.HEAD = new Texture(gl, "robot-head.png");
-  Robot.BODY = new Texture(gl, "robot-body.png");
-  Robot.ARM = new Texture(gl, "robot-arm.png");
-  Robot.LEG = new Texture(gl, "robot-leg.png");
+  Robot.BLUE = {
+    EYE: new Texture(gl, "robot-blue-eye.png"),
+    HEAD: new Texture(gl, "robot-blue-head.png"),
+    BODY: new Texture(gl, "robot-blue-body.png"),
+    ARM: new Texture(gl, "robot-blue-arm.png"),
+    LEG: new Texture(gl, "robot-blue-leg.png")
+  };
+
+  Robot.RED = {
+    EYE: new Texture(gl, "robot-red-eye.png"),
+    HEAD: new Texture(gl, "robot-red-head.png"),
+    BODY: new Texture(gl, "robot-red-body.png"),
+    ARM: new Texture(gl, "robot-red-arm.png"),
+    LEG: new Texture(gl, "robot-red-leg.png")
+  };
 
   Robot.XY = new StaticBuffer(gl, [-0.5, -0.5, -0.5,  0.5,  0.5, -0.5,
                                     0.5, -0.5,  0.5,  0.5, -0.5,  0.5]);
@@ -77,41 +123,81 @@ Robot.init = function(gl) {
 
 Robot.prototype.draw = function(view) {
   var gl = this.gl;
-  this.headModel = [68,  0, 22 + Math.cos(this.tick / 10) * 5,
-                     0, 68, 48,
-                     0,  0,  1];
-  this.leftArmModel = [96,  0, -22,
-                        0, 72,   2 + Math.cos(this.tick / 10) * 5,
-                        0,  0,   1];
-  this.rightArmModel = [96,  0, 80,
-                         0, 72,  2 - Math.cos(this.tick / 10) * 5,
-                         0,  0,  1];
-  this.bodyModel = [132,  0,   0,
-                     0, 100,   2,
-                     0,   0,   1];
-  this.leftLegModel = [60,  0, -20 + Math.sin(this.tick / 10 - Math.PI) * 5,
-                        0, 50, -56 + Math.max(-Math.cos(this.tick / 10) * 5, 0),
-                        0,  0,  1];
-  this.rightLegModel = [60,  0, 44 + Math.sin(this.tick / 10) * 5,
-                         0, 50, -56 + Math.max(Math.cos(this.tick / 10) * 5, 0),
-                         0,  0,  1];
-  Robot.LEG.use();
-  Robot.PROGRAM.use({view: view, model: this.rightLegModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
+  var parts = this.parts;
+
+  var cos = Math.cos(this.tick / 5);
+  var sin = Math.sin(this.tick / 5);
+  var blink = 1 - Math.pow(Math.cos(this.tick / 40), 16);
+  var f = this.facing;
+  var x = this.x;
+  var y = this.y;
+  if (this.walking) {
+    this.x += 16 / 5 * f;
+    this.eyeModel = [26 * f,  0, (34 + 4 * cos) * f + x,
+                      0, 50 * blink, 48 + 2 * Math.abs(sin) - 12.5 + 12.5 * blink + y,
+                      0,  0,  1];
+    this.headModel = [68 * f,  0, (22 + 4 * cos) * f + x,
+                       0, 68, 48 + 2 * Math.abs(sin) + y,
+                       0,  0,  1];
+    this.frontArmModel = [96 * f,  0, -22 * f + x,
+                           0, 72,   2 + 4 * cos + y,
+                           0,  0,   1];
+    this.backArmModel = [96 * f,  0, 80 * f + x,
+                          0, 72,  2 - 4 * cos + y,
+                          0,  0,  1];
+    this.bodyModel = [132 * f,  0, -2 * cos * f + x,
+                       0, 100,   2 - Math.abs(sin) + y,
+                       0,   0,   1];
+    this.frontLegModel = [60 * f,  0, (-20 - 8 * sin) * f + x,
+                           0, 50, -56 + Math.max(-8 * cos, 0) + y,
+                           0,  0,  1];
+    this.backLegModel = [60 * f,  0, (44 + 8 * sin) * f + x,
+                          0, 50, -56 + Math.max(8 * cos, 0) + y,
+                          0,  0,  1];
+  } else {
+    this.eyeModel = [26 * f,  0, 34 * f + x,
+                      0, 50 * blink, 48 - 4 * cos - 12.5 + 12.5 * blink + y,
+                      0,  0,  1];
+    this.headModel = [68 * f,  0, 22 * f + x,
+                       0, 68, 48 - 4 * cos + y,
+                       0,  0,  1];
+    this.frontArmModel = [96 * f,  0, -22 * f + x,
+                           0, 72,   2 + 2 * cos + y,
+                           0,  0,   1];
+    this.backArmModel = [96 * f,  0, 80 * f + x,
+                          0, 72,  2 + 2 * cos + y,
+                          0,  0,  1];
+    this.bodyModel = [132 * f,  0, x,
+                       0, 100,   2 - cos + y,
+                       0,   0,   1];
+    this.frontLegModel = [60 * f,  0, -20 * f + x,
+                           0, 50, -56 + y,
+                           0,  0,  1];
+    this.backLegModel = [60 * f,  0, 44 * f + x,
+                          0, 50, -56 + y,
+                          0,  0,  1];
+  }
+
+  parts.LEG.use();
+  Robot.PROGRAM.use({view: view, model: this.backLegModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  Robot.ARM.use();
-  Robot.PROGRAM.use({view: view, model: this.rightArmModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
+  parts.ARM.use();
+  Robot.PROGRAM.use({view: view, model: this.backArmModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  Robot.BODY.use();
+  parts.BODY.use();
   Robot.PROGRAM.use({view: view, model: this.bodyModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  Robot.HEAD.use();
+  parts.HEAD.use();
   Robot.PROGRAM.use({view: view, model: this.headModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  Robot.LEG.use();
-  Robot.PROGRAM.use({view: view, model: this.leftLegModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
+  parts.EYE.use();
+  Robot.PROGRAM.use({view: view, model: this.eyeModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  Robot.ARM.use();
-  Robot.PROGRAM.use({view: view, model: this.leftArmModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
+  parts.LEG.use();
+  Robot.PROGRAM.use({view: view, model: this.frontLegModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+  parts.ARM.use();
+  Robot.PROGRAM.use({view: view, model: this.frontArmModel, texture: 0}, {xy: Robot.XY, uv: Robot.UV});
   gl.drawArrays(gl.TRIANGLES, 0, 6);
   this.tick++;
 }
